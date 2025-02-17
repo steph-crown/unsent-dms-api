599cde99a3e55db3b5b8bd3d1ee90b927c3a8200
Steph Crown <emmanuelstephen024@gmail.com>
2025-02-17 10:41:44 +0100

    Add swagger

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -14,7 +14,7 @@ const config = {
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
-  synchronize: false,
+  synchronize: true,
   ssl: true,
   applicationName: 'unsent_dms',
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable cors.
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://weoutsideapp.co',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  // swagger setup starts
+  const config = new DocumentBuilder()
+    .setTitle('unsent dms')
+    .setDescription('unsent dms')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    customSiteTitle: 'unsent dms',
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
